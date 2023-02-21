cc5bcad3c1624c58aef2426eac0b4d3e04ef69ba
yaofei zheng <Yaofei.Zheng@dell.com>
2023-02-21 11:09:22 +0800

    Fixes #1

--- background.js
+++ background.js
@@ -18,36 +18,55 @@ browser.menus.create({
     title: "Archive by Year",
     contexts: ["folder_pane"],
     async onclick(info) {
+        // all the console.log() was used to determine the variable type and content.
+        // unlike strong typed C, C++, C# or java, this is bad.
         console.log("onclick--------------------");
         console.log(info.selectedFolder);
-        const objMsgList = await messenger.messages.list(info.selectedFolder);
+        objMsgList = await messenger.messages.list(info.selectedFolder);
 
         console.log(objMsgList);
 
         let ArrayEmailListByYear = [];
+        // get an array of email msg id and year for temporary usage
+        // we can not create folder in the email message loop through
+        // because the `info.selectedFolder.subFolders` was not updated
+        // when new subfolders were created in this onclick callback
+        do {
+            for await (objEmailMsg of objMsgList.messages) {
+                console.log("objEmailMsg start--------------------");
+                let itemYear = objEmailMsg.date.getFullYear();
 
-        for await (objEmailMsg of objMsgList.messages) {
-            console.log("objEmailMsg start--------------------");
-            let itemYear = objEmailMsg.date.getFullYear();
+                let i;
+                for (i = 0; i < ArrayEmailListByYear.length; i++) {
+                    if (ArrayEmailListByYear[i].year === itemYear) {
+                        ArrayEmailListByYear[i].arrayMsgID.push(objEmailMsg.id);
+                        break;
+                    }
+                }
 
-            let i;
-            for (i = 0; i < ArrayEmailListByYear.length; i++) {
-                if (ArrayEmailListByYear[i].year === itemYear) {
-                    ArrayEmailListByYear[i].arrayMsgID.push(objEmailMsg.id);
-                    break;
+                if (i === ArrayEmailListByYear.length) {
+                    ArrayEmailListByYear.push({
+                        year: itemYear,
+                        arrayMsgID: [objEmailMsg.id]
+                    });
                 }
             }
 
-            if (i === ArrayEmailListByYear.length) {
-                ArrayEmailListByYear.push({
-                    year: itemYear,
-                    arrayMsgID: [objEmailMsg.id]
-                });
+            if (null !== objMsgList.id) {
+                console.log(objMsgList.id);
+                objMsgList = await messenger.messages.continueList(objMsgList.id);
+                console.log(objMsgList);
+                console.log(objMsgList.id);
             }
-        }
+            else
+                break;
+        } while (0 !== objMsgList.messages.length);
 
         console.log(ArrayEmailListByYear);
 
+        // loop through the array of email msg id and year
+        // create folder if the year does not exists and
+        // move email to corresponding folder
         for await (it of ArrayEmailListByYear) {
             console.log("create start--------------------");
             console.log(it);
