7b7fd8f7bdc7b400db906e1c806a243e4385ddb0
yaofei zheng <zyf26256@gmail.com>
2023-08-04 23:56:41 +0800

    try to add retention days for emails
    I'm not a web developer, this is my best effort.
    Retention days can be set in preference. So this addon
    will sort emails older than retention days to subfolders only.

--- background.js
+++ background.js
@@ -1,3 +1,14 @@
+function onError(error) {
+    console.log(error);
+}
+
+var retention_days = 0;
+
+browser.storage.local.get("retention").then((result) => {
+    let objTest = Object.values(result);
+    retention_days = objTest[0];
+}, onError);
+
 function findFolder(array, string2find) {
     for (item of array) {
         if (item.name === string2find)
@@ -18,13 +29,25 @@ browser.menus.create({
     title: "Archive by Year",
     contexts: ["folder_pane"],
     async onclick(info) {
-        // all the console.log() was used to determine the variable type and content.
+
+        // console.log("retention days: " + retention_days);
+        if(typeof retention_days === 'undefined') {
+            retention_days = 0;
+        } else if (retention_days === null) {
+            retention_days = 0;
+        } else if (Number.isNaN(retention_days)) {
+            retention_days = 0;
+        }
+        // console.log("retention days: " + retention_days);
+
+        // all the console.log() were used to determine the variable type and content.
         // unlike strong typed C, C++, C# or java, this is bad.
-        console.log("onclick--------------------");
-        console.log(info.selectedFolder);
+
+        // console.log("onclick--------------------");
+        // console.log(info.selectedFolder);
         objMsgList = await messenger.messages.list(info.selectedFolder);
 
-        console.log(objMsgList);
+        // console.log(objMsgList);
 
         let ArrayEmailListByYear = [];
         // get an array of email msg id and year for temporary usage
@@ -33,47 +56,60 @@ browser.menus.create({
         // when new subfolders were created in this onclick callback
         do {
             for await (objEmailMsg of objMsgList.messages) {
-                console.log("objEmailMsg start--------------------");
+                // console.log("objEmailMsg start--------------------");
                 let itemYear = objEmailMsg.date.getFullYear();
 
                 let i;
                 for (i = 0; i < ArrayEmailListByYear.length; i++) {
                     if (ArrayEmailListByYear[i].year === itemYear) {
-                        ArrayEmailListByYear[i].arrayMsgID.push(objEmailMsg.id);
+                        if (0 === retention_days) {
+                            ArrayEmailListByYear[i].arrayMsgID.push(objEmailMsg.id);
+                        }
+                        else {
+                            ArrayEmailListByYear[i].msgs.push(objEmailMsg);
+                        }
                         break;
                     }
                 }
 
                 if (i === ArrayEmailListByYear.length) {
-                    ArrayEmailListByYear.push({
-                        year: itemYear,
-                        arrayMsgID: [objEmailMsg.id]
-                    });
+                    if (0 === retention_days) {
+                        ArrayEmailListByYear.push({
+                            year: itemYear,
+                            arrayMsgID: [objEmailMsg.id]
+                        });
+                    }
+                    else {
+                        ArrayEmailListByYear.push({
+                            year: itemYear,
+                            msgs: [objEmailMsg]
+                        });
+                    }
                 }
             }
 
             if (null !== objMsgList.id) {
-                console.log(objMsgList.id);
+                // console.log(objMsgList.id);
                 objMsgList = await messenger.messages.continueList(objMsgList.id);
-                console.log(objMsgList);
-                console.log(objMsgList.id);
+                // console.log(objMsgList);
+                // console.log(objMsgList.id);
             }
             else
                 break;
         } while (0 !== objMsgList.messages.length);
 
-        console.log(ArrayEmailListByYear);
+        // console.log(ArrayEmailListByYear);
 
         // loop through the array of email msg id and year
         // create folder if the year does not exists and
         // move email to corresponding folder
         for await (it of ArrayEmailListByYear) {
-            console.log("create start--------------------");
-            console.log(it);
+            // console.log("create start--------------------");
+            // console.log(it);
             let strNewEmailFolderName = info.selectedFolder.name + "." + it.year;
             let objectNewFolder;
             let findret = findFolder(info.selectedFolder.subFolders, strNewEmailFolderName);
-            console.log(findret);
+            // console.log(findret);
             if (findret.ret) {
                 objectNewFolder = findret.folder;
             } else {
@@ -83,7 +119,21 @@ browser.menus.create({
                 );
             }
 
-            await messenger.messages.move(it.arrayMsgID, objectNewFolder);
+            if (0 === retention_days) {
+                await messenger.messages.move(it.arrayMsgID, objectNewFolder);
+            }
+            else {
+                let arrayMsgID = [];
+                for (msg of it.msgs) {
+                    // console.log(msg.date/1000);
+                    // console.log(Date.now()/1000);
+                    // console.log(Date.now()/1000 - msg.date/1000);
+                    if ((Date.now() - msg.date)/1000 > retention_days * 24 * 60 * 60) {
+                        arrayMsgID.push(msg.id);
+                    }
+                }
+                await messenger.messages.move(arrayMsgID, objectNewFolder);
+            }
         }
     },
 });
